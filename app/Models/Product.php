2fdd588c4320518192782d7346166a1768707902
v-e-y <v.eduard.y@gmail.com>
2022-12-31 11:13:20 +0200

    add models, write relations

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Category;
+use App\Models\Condition;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * Relations
+     */
+
+    public function condition()
+    {
+        return $this->belongsTo(Condition::class);
+    }
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
